34cf9c51f968b897b29c16b62ebc18e528830e35
clough <james.clough@hp.com>
2019-04-12 08:06:44 -0600

    Clean up comments and add license to source files

--- els-f280049c/ControlPanel.cpp
+++ els-f280049c/ControlPanel.cpp
@@ -1,3 +1,29 @@
+// Clough42 Electronic Leadscrew
+// https://github.com/clough42/electronic-leadscrew
+//
+// MIT License
+//
+// Copyright (c) 2019 James Clough
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
 #include "ControlPanel.h"
 
 
@@ -209,12 +235,8 @@ KEY_REG ControlPanel :: getKeys()
 
 void ControlPanel :: refresh()
 {
-    GpioDataRegs.GPASET.bit.GPIO2 = 1;
-
     decomposeRPM();
     decomposeValue();
-
-    GpioDataRegs.GPACLEAR.bit.GPIO2 = 1;
 
     sendData();
 }
--- els-f280049c/main.cpp
+++ els-f280049c/main.cpp
@@ -1,24 +1,45 @@
+// Clough42 Electronic Leadscrew
+// https://github.com/clough42/electronic-leadscrew
 //
-// Included Files
+// MIT License
 //
-#include "F28x_Project.h"     // Device Headerfile and Examples Include File
+// Copyright (c) 2019 James Clough
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+
+#include "F28x_Project.h"
 #include "ControlPanel.h"
 #include "StepperDrive.h"
 #include "Encoder.h"
 #include "Configuration.h"
 #include "Core.h"
 #include "UserInterface.h"
-#include <stdio.h>
 
-//
-// Function Prototypes statements for functions found within this file.
-//
+
 __interrupt void cpu_timer0_isr(void);
-
 
 
 //
 // DEPENDENCY INJECTION
+//
+// Declare all of the main components and wire them together
 //
 
 // Feed table factory
@@ -40,10 +61,6 @@ Core core(&encoder, &stepperDrive);
 UserInterface userInterface(&controlPanel, &core, &feedTableFactory);
 
 
-
-//
-// Main
-//
 void main(void)
 {
 #ifdef _FLASH
@@ -57,139 +74,68 @@ void main(void)
     InitFlash();
 #endif
 
-
-    //
-    // Step 1. Initialize System Control:
+    // Initialize System Control:
     // PLL, WatchDog, enable Peripheral Clocks
-    // This example function is found in the F2806x_SysCtrl.c file.
-    //
     InitSysCtrl();
 
-    //
-    // Step 2. Initalize GPIO:
-    // This example function is found in the F2806x_Gpio.c file and
-    // illustrates how to set the GPIO to it's default state.
-    //
-    // InitGpio();  // Skipped for this example
-
-    //
-    // Step 3. Clear all interrupts and initialize PIE vector table:
     // Disable CPU interrupts
-    //
     DINT;
 
-    //
     // Initialize the PIE control registers to their default state.
-    // The default state is all PIE interrupts disabled and flags
-    // are cleared.
-    // This function is found in the F2806x_PieCtrl.c file.
-    //
     InitPieCtrl();
 
-    //
     // Disable CPU interrupts and clear all CPU interrupt flags
-    //
     IER = 0x0000;
     IFR = 0x0000;
 
-    //
-    // Initialize the PIE vector table with pointers to the shell Interrupt
-    // Service Routines (ISR).
-    // This will populate the entire table, even if the interrupt
-    // is not used in this example.  This is useful for debug purposes.
-    // The shell ISR routines are found in F2806x_DefaultIsr.c.
-    // This function is found in F2806x_PieVect.c.
-    //
+    // Initialize the PIE vector table with pointers to shell Interrupt
+    // Service Routines (ISR) to help with debugging.
     InitPieVectTable();
 
-    //
-    // Interrupts that are used in this example are re-mapped to
-    // ISR functions found within this file.
-    //
-    EALLOW;    // This is needed to write to EALLOW protected registers
+    // Set up the CPU0 timer ISR
+    EALLOW;
     PieVectTable.TIMER0_INT = &cpu_timer0_isr;
-    EDIS;      // This is needed to disable write to EALLOW protected registers
+    EDIS;
 
-    //
-    // Step 4. Initialize the Device Peripheral. This function can be
-    //         found in F2806x_CpuTimers.c
-    //
+    // initialize the CPU timer
     InitCpuTimers();   // For this example, only initialize the Cpu Timers
-
-    //
-    // Configure CPU-Timer 0 to interrupt every 500 milliseconds:
-    // 80MHz CPU Freq, 50 millisecond Period (in uSeconds)
-    //
     ConfigCpuTimer(&CpuTimer0, CPU_CLOCK_MHZ, STEPPER_CYCLE_US);
 
-    //
-    // To ensure precise timing, use write-only instructions to write to the
-    // entire register. Therefore, if any of the configuration bits are changed
-    // in ConfigCpuTimer and InitCpuTimers (in F2806x_CpuTimers.h), the
-    // below settings must also be updated.
-    //
-
-    //
     // Use write-only instruction to set TSS bit = 0
-    //
     CpuTimer0Regs.TCR.all = 0x4001;
 
-    //
-    // Initialize the system
-    //
+    // Initialize components
     controlPanel.init();
     stepperDrive.init();
     encoder.init();
     userInterface.init();
 
-    // GPIO 7 as an indicator
+    // Set up GPIO7 as a timing indicator for the logic analyzer
     EALLOW;
     GpioCtrlRegs.GPAMUX1.bit.GPIO7 = 0;
     GpioCtrlRegs.GPADIR.bit.GPIO7 = 1;
     GpioDataRegs.GPACLEAR.bit.GPIO7 = 1;
-    GpioCtrlRegs.GPAMUX1.bit.GPIO2 = 0;
-    GpioCtrlRegs.GPADIR.bit.GPIO2 = 1;
-    GpioDataRegs.GPACLEAR.bit.GPIO2 = 1;
     EDIS;
 
-    //
-    // Step 5. User specific code, enable interrupts:
-    //
-
-    //
     // Enable CPU INT1 which is connected to CPU-Timer 0
-    //
     IER |= M_INT1;
 
-    //
     // Enable TINT0 in the PIE: Group 1 interrupt 7
-    //
     PieCtrlRegs.PIEIER1.bit.INTx7 = 1;
 
-    //
     // Enable global Interrupts and higher priority real-time debug events
-    //
     EINT;   // Enable Global interrupt INTM
     ERTM;   // Enable Global realtime interrupt DBGM
 
-    //
-    // Start up
-    //
-
-    //
-    // Step 6. IDLE loop. Just sit and loop forever (optional)
-    //
+    // User interface loop
     for(;;) {
         userInterface.loop();
-
         DELAY_US(10000); // update at 100Hz-ish
-    };
+    }
 }
 
 
-//
-// cpu_timer0_isr -
-//
+// CPU Timer 0 ISR
 __interrupt void
 cpu_timer0_isr(void)
 {
@@ -198,6 +144,7 @@ cpu_timer0_isr(void)
     // flag entrance to ISR for timing
     GpioDataRegs.GPASET.bit.GPIO7 = 1;
 
+    // service the Core engine ISR, which in turn services the StepperDrive ISR
     core.ISR();
 
     // flag exit from ISR for timing
@@ -209,7 +156,4 @@ cpu_timer0_isr(void)
     PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
 }
 
-//
-// End of File
-//
 
